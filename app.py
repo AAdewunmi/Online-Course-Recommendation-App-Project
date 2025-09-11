19940e509df8ac6433f53b52cc22a65c42f8d168
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-09-11 12:33:29 +0100

    Implement home() function

--- app.py
+++ app.py
@@ -32,8 +32,7 @@ def cosine__sim_mat(cvmat):
 
 
 def recommend_course(df, title, cosine_mat, numrec):
-    course_index = pd.Series(df.index, 
-                             index=df['course_title']).drop_duplicates()
+    course_index = pd.Series(df.index, index=df['course_title']).drop_duplicates()
     index = course_index[title]
     scores = list(enumerate(cosine_mat[index]))
     sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
@@ -65,4 +64,51 @@ def home():
         my_dict = request.form
         titlename = my_dict['course']
         print(titlename)
+        try:
+            df = read_data()
+            df = get_clean_title(df)
+            cvmat = get_cosine_mat(df)
+            num_rec = 6
+            cosine_mat = cosine__sim_mat(cvmat)
+            recdf = recommend_course(df, titlename, cosine_mat, num_rec)
+            course_url, course_title, course_price = extracted_features(recdf)
+            dictmap = dict(zip(course_title, course_url, course_price))
+            if len(dictmap) != 0:
+                return render_template('index.html', 
+                                       coursemap=dictmap,
+                                       coursename=titlename,
+                                       showtitle=True)
+            else:
+                return render_template('index.html',
+                                       showerror=True,
+                                       coursename=titlename) 
+        except Exception:
+            resultdf = search_term(titlename, df)
+            if resultdf.shape[0] > 6:
+                resultdf = resultdf.head(6)
+                course_url, course_title, course_price = extracted_features(
+                    resultdf)
+                coursemap = dict(zip(course_title, course_url, course_price))
+                if len(coursemap) != 0:
+                    return render_template('index.html', 
+                                           coursemap=coursemap,
+                                           coursename=titlename,
+                                           showtitle=True)
+                else:
+                    return render_template('index.html',
+                                           showerror=True,
+                                           coursename=titlename)
+            else:
+                course_url, course_title, course_price = extracted_features(
+                    resultdf)
+                coursemap = dict(zip(course_title, course_url, course_price))
+                if len(coursemap) != 0:
+                    return render_template('index.html', 
+                                           coursemap=coursemap,
+                                           coursename=titlename,
+                                           showtitle=True)
+                else:
+                    return render_template('index.html',
+                                           showerror=True,
+                                           coursename=titlename)
     return render_template('index.html')
