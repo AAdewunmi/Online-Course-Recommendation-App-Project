7010f767ef055bcb79743e396f3153ec1be41f7a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-09-12 11:38:57 +0100

    Implement year_wise_profit() function

--- dashboard.py
+++ dashboard.py
@@ -15,3 +15,29 @@ def get_subjects_per_level(df):
     ans_values = list(dict(df.groupby(['subject'])['level'].value_counts()).values())
     complete_dict = dict(zip(all_labels, ans_values))
     return complete_dict
+
+
+def year_wise_profit(df):
+    df['price'] = df['price'].str.replace('TRUE|Free', '0')
+    df['price'] = df['price'].astype('float')
+    df['profit'] = df['price'] * df['num_subscribers']
+
+    # Converting the time column to year,month,day and many more
+    df['published_date'] = df['published_timestamp'].apply(
+        lambda x: x.split('T')[0])
+
+    # dropping of that index which has '3 hours' as time
+    df = df.drop(df.index[2066])
+
+    # converting the published date to pandas datetime object
+    df['published_date'] = pd.to_datetime(
+        df['published_date'], format="%Y-%m-%d")
+    df['Year'] = df['published_date'].dt.year
+    df['Month'] = df['published_date'].dt.month
+    df['Day'] = df['published_date'].dt.day
+    df['Month_name'] = df['published_date'].dt.month_name()
+    profitmap = dict(df.groupby(['Year'])['profit'].sum())
+    subscribersmap = dict(df.groupby(['Year'])['num_subscribers'].sum())
+    profitmonthwise = dict(df.groupby(['Month_name'])['profit'].sum())
+    monthwisesub = dict(df.groupby(['Month_name'])['num_subscribers'].sum())
+    return profitmap, subscribersmap, profitmonthwise, monthwisesub
